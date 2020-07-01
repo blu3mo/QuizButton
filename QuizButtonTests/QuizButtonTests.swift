30d4d5b9f7fce21425307d60a2b9f59be749be13
Bluemo <shutaro.aoyama@gmail.com>
2020-07-02 01:15:36 +0900

    Created Simple Tests
    Not sure if it is a correct way

--- QuizButtonTests/QuizButtonTests.swift
+++ QuizButtonTests/QuizButtonTests.swift
@@ -7,28 +7,90 @@
 //
 
 import XCTest
+import MultipeerConnectivity
 @testable import QuizButton
 
+class MCServiceStub: ConnectionServiceProtocol{
+    var model: JudgeModelProtocol!
+    
+    var returningMessage: Message!
+    
+    init(returningMessage: Message) {
+        self.returningMessage = returningMessage
+    }
+    
+    func inject(model: JudgeModelProtocol) {
+        self.model = model
+    }
+    
+    var peerId: MCPeerID!
+    var mcSession: MCSession!
+}
+
+extension MCServiceStub: JudgeModelConnectionOutput {
+    
+    var isConnected: Bool {
+        return true
+    }
+    
+    func sendMessage(message: Message) {
+        model.reactMessage(message: Message.triedDate(Date() + 10000))
+        return
+    }
+    
+    func advertise() {
+        return
+    }
+    
+    func getBrowser() -> UIViewController {
+        return UIViewController()
+    }
+    
+}
+
 class QuizButtonTests: XCTestCase {
+    
+    var homeViewController: HomeViewController!
+    var presenter: HomePresenter!
+    var model: JudgeModel!
+    var connectionService: MCServiceStub!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        homeViewController = (UIStoryboard(name: "Home", bundle: nil).instantiateInitialViewController() as! HomeViewController)
+        
+        presenter = HomePresenter(view: homeViewController)
+        connectionService = MCServiceStub(returningMessage: .youWin)
+
+        model = JudgeModel(presenter: presenter, connectionService: connectionService)
+        
+        presenter.inject(model: model)
+        connectionService.inject(model: model)
+        
+        homeViewController.inject(presenter: presenter)
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func reactionToAnswerButton_WhenWinningAlways() {
+        connectionService.returningMessage = Message.youWin
+        homeViewController.answerButtonTapped(UIButton())
+        XCTAssertEqual(homeViewController.view.backgroundColor, UIColor.red)
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func reactionToAnswerButton_WhenWinningByDate() {
+        connectionService.returningMessage = Message.triedDate(Date() + 100)
+        homeViewController.answerButtonTapped(UIButton())
+        XCTAssertEqual(homeViewController.view.backgroundColor, UIColor.red)
     }
-
+    
+    func reactionToAnswerButton_WhenLosingByDate() {
+        connectionService.returningMessage = Message.triedDate(Date() - 100)
+        homeViewController.answerButtonTapped(UIButton())
+        XCTAssertEqual(homeViewController.view.backgroundColor, UIColor.blue)
+    }
 }
